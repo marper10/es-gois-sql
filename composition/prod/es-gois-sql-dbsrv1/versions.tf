ce7afa2a9d9b570304b6eb13483505874c67aa1b
marper10 <maria.perez@dbschenker.com>
2024-10-22 11:07:55 -0600

    INC008859966 New Build

--- composition/prod/es-gois-sql-dbsrv1/versions.tf
+++ composition/prod/es-gois-sql-dbsrv1/versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 1.0"
   required_providers {
-    grafana = {
-      source = "grafana/grafana"
-    }
+    #grafana = {
+    #  source = "grafana/grafana"
+    #}
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
